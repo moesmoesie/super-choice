254d4b0a820bbbd0955327dd98d7595d40b09f24
Mustafa <mndarwesh@protonmail.com>
2022-01-14 17:52:31 +0100

    Connect to cms

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,60 @@
+import groq from "groq"
+import { client,urlFor } from "../lib/client"
 
-export default function Home() {
+export default function Home({ homePage }) {
+
   return (
     <div>
 
       <div className=" h-screen w-full relative">
-
         <img className="absolute w-full h-full object-cover"
-          src="/foto1.png"
+          src={urlFor(homePage.landingImage.image.asset)}
         />
 
         <div className="absolute bottom-[5vh]  w-full flex flex-col items-center">
-          <h1 className="text-center text-white text-6xl md:text-9xl font-black mb-5 md:mb-12">Super Choice</h1>
-
+          <h1 className="text-center text-white text-6xl md:text-9xl font-black mb-5 md:mb-12">
+            {homePage.title}
+          </h1>
 
           <div className="flex flex-col md:flex-row items-center gap-5 md:mb-3">
             <a href="#" className="bg-primary4 button">
-              Bekijk ons assortiment
+              {homePage.callToAction.primary.text}
             </a>
 
             <a href="#" className="button">
-              Lees over ons
+              {homePage.callToAction.secondary.text}
             </a>
           </div>
 
           <img className="w-full max-w-2xl hidden md:block"
-            src="/finestSelection.svg" />
+            src="/finestSelection.svg"/>
 
         </div>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+
+  const query = groq`
+    *[_type == "homePage"][0]{
+        _id,
+        callToAction{
+                primary{text, "url":url->slug},
+                secondary{text, "url": url->slug}
+        },
+        seo,
+        landingImage,
+        title,
+        navigation[]{richImage,title,"url":url->slug},
+    }
+  `
+  const homePage = await client.fetch(query)
+
+  return {
+    props: {
+      "homePage": homePage
+    }
+  }
 }
