3887ba11c65ddc208925479ad28505d55190bb59
Mustafa <mndarwesh@protonmail.com>
2022-01-14 17:15:51 +0100

    Create landing section of home page

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary1": "#53D8FB",
+        "primary2": "#66C3FF",
+        "primary3": "#18A0FB",
+        "primary4": "#007ACC",
+        "primary5": "#142C4B",
+        "secondary1": "#FFB01F"
+      },
+    },
+  },
+  plugins: [],
+}
