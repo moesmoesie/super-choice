142ccff344f4e74b2047f0566d63c42a86d00cd9
Mustafa <mndarwesh@protonmail.com>
2022-01-15 12:11:16 +0100

    Create navigation section of home page

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home({ homePage }) {
 
   return (
     <div>
-
+      {/* Landing Section */}
       <div className=" h-screen w-full relative">
         <img className="absolute w-full h-full object-cover"
           src={urlForImage(homePage.landingImage.image.asset)}
@@ -21,20 +21,64 @@ export default function Home({ homePage }) {
             <a href="#" className="bg-primary4 button">
               {homePage.callToAction.primary.text}
             </a>
-
             <a href="#" className="button">
               {homePage.callToAction.secondary.text}
             </a>
           </div>
 
           <img className="w-full max-w-2xl hidden md:block"
-            src="/finestSelection.svg"/>
-
+            src="/finestSelection.svg" />
         </div>
       </div>
+
+      {/* Navigation Section */}
+      {buildNavigation(homePage)}
     </div>
   )
 }
+
+function buildNavigation(homePage) {
+  function getRowSpan(index) {
+    if ([0, 3].includes(index)) {
+      return "md:row-span-2"
+    } else {
+      return "md:row-span-1"
+    }
+  }
+
+  function getColumnSpan(index) {
+    if ([0, 4, 5].includes(index)) {
+      return "md:col-span-2"
+    } else {
+      return "md:col-span-1"
+    }
+  }
+
+
+  return <div className="grid grid-cols-1 grid-rows-[repeat(6,20rem)] md:grid-rows-[repeat(4,20rem)] md:grid-cols-3">
+    {homePage.navigation.map((element, index) => {
+      return (
+        <div className={`${getRowSpan(index)} ${getColumnSpan(index)} grid place-items-center relative overflow-hidden group`}
+          key={element.title}
+        >
+
+          <img
+            className="absolute w-full h-full object-cover group-hover:scale-110 transition-transform duration-1000"
+            src={urlForImage(element.richImage.image.asset)}
+          />
+
+          <div
+            className="absolute w-full h-full bg-black opacity-60 group-hover:opacity-10 transition-opacity duration-1000"
+          />
+
+          <h2 className="pointer-events-none z-10 text-white font-bold text-3xl md:text-4xl drop-shadow-2xl">{element.title}</h2>
+
+        </div>
+      )
+    })}
+  </div>
+}
+
 
 export async function getStaticProps(context) {
 
