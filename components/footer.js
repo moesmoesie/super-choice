1d5ea6ff32cd77daf28653cc5ca4d92152f4deff
Mustafa <mustafa@mustafas-mbp.home>
2022-01-18 22:12:19 +0100

    Rework footer

--- components/footer.js
+++ components/footer.js
@@ -7,77 +7,60 @@ import { FaFax } from "@react-icons/all-files/fa/FaFax";
 
 export default function Footer({ data }) {
     return (
-        <footer className="grid md:grid-cols-2" >
-            <div className="w-full py-12 px-5 lg:px-12 bg-primary4 grid lg:grid-cols-2">
-                <div>
-                    <Image className="w-16 aspect-square relative mb-4" asset={data.logo.asset} sizes={[500]} />
-                    <p className="text-white mb-6 lg:col-start-1">
-                        2021
-                        <span className="font-bold"> Super Choice.</span>
-                        <br />
-                        All rights reserved
+        <footer className="grid lg:grid-cols-2">
+            <div className="bg-primary4 px-6">
+                <div className="grid lg:grid-cols-2 py-12 max-w-xl lg:mx-auto">
+
+                    {/* Logo */}
+                    <Image className="w-16 aspect-square relative mb-4" asset={data.logo.asset} sizes={[300]}/>
+
+                    <p className="text-white mb-4 lg:col-start-1 lg:mb-0 lg:self-end">
+                        © 2021 Super Choice. <br/> All rights reserved
                     </p>
-                </div>
 
-
-                <div className="lg:col-start-2 lg:row-start-1">
-                    <p className="font-bold text-white uppercase mb-2">
+                    <p className="text-white lg:col-start-2 lg:row-start-1 lg:self-center uppercase mb-1 pb-0 font-bold">
                         Links
                     </p>
 
-                    <ul className="text-white w-full  flex gap-x-4 gap-y-2 flex-wrap">
+                    <ul className="w-full flex lg:grid lg:grid-cols-2 text-white gap-1 gap-x-3 lg:gap-x-4 flex-wrap">
                         {data.footerLinks.map((link, index) => {
                             return <li key={index}>{link}</li>
                         })}
                     </ul>
                 </div>
-
             </div>
+           
 
-            <div className="w-full py-12 px-5 md:pl-12 bg-primary5 grid lg:grid-cols-2">
+            <div className="bg-primary5 px-6">
+                <div className="grid lg:grid-cols-2 py-12 lg:mx-auto max-w-xl">
+                    <div className="w-16 aspect-square mb-4 hidden lg:block lg:col-start-2"/>
+                        <p className="text-white lg:col-start-1 lg:row-start-1 lg:self-center uppercase mb-1 pb-0 font-bold">
+                            Contact
+                        </p>
 
-                <div>
-                    <p className="font-bold text-white uppercase mb-4">
-                        Contact
-                    </p>
+                        <ul className="w-full grid text-white gap-1 mb-6">
+                            <li>{data.contact.addressLine1}</li>
+                            <li>{data.contact.postalCode} {data.contact.city} </li>
+                            <li>{data.contact.country}</li>
+                        </ul>
 
-                    <p className="text-white mb-2">
-                        {data.contact.addressLine1} 
-                    </p>
-
-                    <p className="text-white mb-2">
-                    {data.contact.postalCode} {data.contact.city} 
-                    </p>
-
-                    <p className="text-white mb-2">
-                    {data.contact.country}
-                    </p>
-
-                </div>
-
-                <div>
-                    <p className="font-bold text-white uppercase mb-4 invisible">
-                        Contact
-                    </p>
-
-                    <div className="flex gap-x-4 mb-2">
-                        <AiOutlineInstagram className="text-primary2" size={24} />
-                        <AiOutlineLinkedin className="text-primary2" size={24} />
-                        <AiOutlineFacebook className="text-primary2" size={24} />
+                        <ul className="w-full flex  text-white gap-1 lg:gap-x-2 flex-col">
+                            <li className="flex gap-4">
+                                <AiOutlineInstagram className="text-primary2" size={20}/>
+                                <AiOutlineLinkedin className="text-primary2" size={20}/>
+                                <AiOutlineFacebook className="text-primary2" size={20}/>
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <AiTwotonePhone className="text-primary2"/>
+                                {data.contact.phone}
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <FaFax className="text-primary2"/>
+                                {data.contact.fax}
+                            </li>
+                        </ul>
                     </div>
-
-                    <p className="text-white mb-2  flex items-center">
-                        <AiTwotonePhone size={20} className="text-primary2 w-5 inline-block mr-3" />
-                        {data.contact.phone}
-                    </p>
-
-                    <p className="text-white flex items-center">
-                        <FaFax size={20} className="text-primary2 w-5 inline-block mr-3" />
-                        {data.contact.fax}
-                    </p>
                 </div>
-
-            </div>
         </footer>
     )
 }
