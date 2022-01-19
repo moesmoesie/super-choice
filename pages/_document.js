3f08a659544e6232fd1467f63cb5f3bd5a9894cd
Mustafa <mndarwesh@protonmail.com>
2022-01-19 13:37:47 +0100

    Load fonts locally

---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    const bodyFontWeights = [400,700]
+    const headerFontWeights = [700, 800, 900]
+
+    return (
+        <Html>
+            <Head>
+                {bodyFontWeights.map((element,index) => {
+                    return <link
+                        key={index}
+                        rel="preload"
+                        href={`/fonts/montserrat/montserrat-${element}.woff2`}
+                        as="font"
+                        crossOrigin=""
+                    />
+                })}
+
+                {headerFontWeights.map((element,index) => {
+                    return <link
+                        key={index}
+                        rel="preload"
+                        href={`/fonts/mulish/mulish-${element}.woff2`}
+                        as="font"
+                        crossOrigin=""
+                    />
+                })}
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
