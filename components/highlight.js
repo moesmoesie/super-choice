3139f683cc098f1c75dc10af6df82e8aa02edce9
Mustafa <mndarwesh@protonmail.com>
2022-01-22 23:03:55 +0100

    Create sustainibiliy page

--- components/highlight.js
+++ components/highlight.js
@@ -1,21 +1,52 @@
 import Link from "next/link"
+import Image from "./image"
 
 export default function Highlight(props) {
-    const {highlight} = props
+    const { highlight } = props
+    const hasCta = highlight.callToAction != undefined
+    const hasImage = highlight?.image?.asset != undefined
+
     return (
         <div className={`md:wrapper lg:max-w-none ${props.className}`}>
             <div className="wrapper py-12 bg-primary3  md:rounded-md">
-                <p className="text-4xl text-white mb-6 md:text-5xl">
-                    {highlight?.title}
-                </p>
-                <p className="text-white mb-8 md:columns-2">
-                    {highlight?.text}
-                </p>
-                <Link href='#'>
-                    <a className="button bg-primary5 text-xs">
-                        {highlight?.callToAction.text}
-                    </a>
-                </Link>
+                <div className="flex gap-7 items-center">
+                    <div className="flex-1">
+
+                        <p className="text-4xl font-bold text-white mb-6 md:text-5xl">
+                            {highlight?.title}
+                        </p>
+                        <p className={`text-white  ${hasImage ? '' : 'md:columns-2'}`}>
+                            {highlight?.text}
+                        </p>
+
+                        {hasCta && (
+                            <div className="pt-8">
+                                <Link href='#'>
+                                    <a className="button bg-primary5 text-xs">
+                                        {highlight?.callToAction.text}
+                                    </a>
+                                </Link>
+                            </div>
+                        )
+                        }
+                    </div>
+
+                    {hasImage && (
+                        <div className="relative hidden md:block w-44 h-44 rounded-full overflow-hidden bg-red-300">
+                            <Image
+                                className='relative h-full w-full md:rounded-md overflow-hidden'
+                                asset={highlight.image.asset}
+                                objectFit='object-cover'
+                                placeholder={highlight.image.metadata.lqip}
+                                sizes={[600]}
+                            />
+                        </div>
+                    )}
+
+                </div>
+
+
+
             </div>
         </div>
     )
--- pages/sustainability/index.js
+++ pages/sustainability/index.js
@@ -1,32 +1,65 @@
-import {getGlobalData, getClient} from '../../lib/sanity/sanity.server'
+import { getGlobalData, getClient } from '../../lib/sanity/sanity.server'
 import React from "react"
 import Layout from "../../components/layout"
 import groq from 'groq'
+import Image from '../../components/image'
+import Highlight from '../../components/highlight'
 
 export default function Home({ sustainabilityPage, global, locale }) {
     return (
         <Layout data={global}>
-            <div className="h-full w-full grid place-items-center">
-                <h1 className="text-black">{sustainabilityPage.title}</h1>
-            </div>
+                <div className="relative w-full md:wrapper mt-20 mb-12 h-96 overflow-hidden">
+                    <Image
+                        className='relative h-full w-full md:rounded-md overflow-hidden'
+                        asset={sustainabilityPage.landingImage.asset}
+                        objectFit='object-cover'
+                        placeholder={sustainabilityPage.landingImage.metadata.lqip}
+                        sizes={[600, 1200, 1800, 2400]}
+                    />
+                </div>
+
+                <h1 className="wrapper mt-12 mb-12 w-full text-4xl md:text-6xl lg:text-7xl text-primary4 font-black">
+                    {sustainabilityPage.title}
+                </h1>
+
+                <p className='wrapper mb-12 lg:columns-2'>
+                    {sustainabilityPage.landingText}
+                </p>
+
+                <Highlight
+                    className="md:mb-16"
+                    highlight={sustainabilityPage.highlight}
+                />
         </Layout>
     )
 }
 
 export async function getStaticProps(context) {
-    const {locale,defaultLocale} = context
+    const { locale, defaultLocale } = context
 
     const getQuery = (locale) => groq`
         *[_type == "sustainabilityPage" && language->languageCode == '${locale}'][0]{
             _id,
             title,
-            'locale' : language->languageCode
+            'locale' : language->languageCode,
+            'highlight' : highlight{
+            ...,
+            'image' : image{
+                ...,
+                'metadata': asset->metadata
+            }
+            },
+            'landingImage' : landingImage{
+            ...,
+            'metadata': asset->metadata
+            },
+            landingText
         }
     `
 
     var sustainabilityPage = await getClient(context?.preview).fetch(getQuery(locale))
 
-    if(!sustainabilityPage){
+    if (!sustainabilityPage) {
         sustainabilityPage = await getClient(context?.preview).fetch(getQuery(defaultLocale))
     }
 
