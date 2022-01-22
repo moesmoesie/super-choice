8d2867c2599158736d901965972b19dc3012fb99
Mustafa <mndarwesh@protonmail.com>
2022-01-22 19:05:25 +0100

    Change background color highlight

--- components/highlight.js
+++ components/highlight.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Highlight(props) {
     const {highlight} = props
     return (
-        <div className={`${props.className} bg-primary2 wrapper py-12 rounded-md place-items-start`}>
+        <div className={`${props.className} bg-primary3 wrapper py-12 rounded-md place-items-start`}>
             <p className="text-4xl text-white mb-6 md:text-5xl">
                 {highlight?.title}
             </p>
