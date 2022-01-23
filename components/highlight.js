8d2d9cca8fc490b7b6ebc778a8e8e46afa3be9e1
Mustafa <mndarwesh@protonmail.com>
2022-01-23 13:39:12 +0100

    Refactor highlight component

--- components/highlight.js
+++ components/highlight.js
@@ -8,46 +8,76 @@ export default function Highlight(props) {
 
     return (
         <div className={`md:wrapper lg:max-w-none ${props.className}`}>
-            <div className="wrapper py-12 bg-primary3  md:rounded-md">
+            <div className="wrapper py-12 bg-primary3 md:rounded-md">
                 <div className="flex gap-7 items-center">
                     <div className="flex-1">
-
-                        <p className="text-4xl font-bold text-white mb-6 md:text-5xl">
+                        <HeadlineTitle className="mb-6">
                             {highlight?.title}
-                        </p>
-                        <p className={`text-white  ${hasImage ? '' : 'md:columns-2'}`}>
+                        </HeadlineTitle>
+
+                        <HighlightText hasImage={hasImage}>
                             {highlight?.text}
-                        </p>
+                        </HighlightText>
 
                         {hasCta && (
                             <div className="pt-8">
-                                <Link href='#'>
-                                    <a className="button bg-primary5 text-xs">
-                                        {highlight?.callToAction.text}
-                                    </a>
-                                </Link>
+                                <HighlightCta cta={highlight.callToAction} />
                             </div>
-                        )
-                        }
+                        )}
                     </div>
 
                     {hasImage && (
-                        <div className="relative hidden md:block w-44 h-44 rounded-full overflow-hidden bg-red-300">
-                            <Image
-                                className='relative h-full w-full md:rounded-md overflow-hidden'
-                                asset={highlight.image.asset}
-                                objectFit='object-cover'
-                                placeholder={highlight.image.metadata.lqip}
-                                sizes={[600]}
-                            />
-                        </div>
+                        <HighlightImage className="w-44 h-44" image={highlight.image} />
                     )}
-
                 </div>
-
-
-
             </div>
         </div>
+    )
+}
+
+const HighlightText = ({ className, children, hasImage }) => {
+    return (
+        <>
+            <p className={`text-white  ${hasImage ? '' : 'md:columns-2'} ${className}`}>
+                {children}
+            </p>
+        </>
+    )
+}
+
+const HighlightCta = ({ className, cta }) => {
+    return (
+        <>
+            <Link href='#'>
+                <a className={`button bg-primary5 text-xs ${className}`}>
+                    {cta.text}
+                </a>
+            </Link>
+        </>
+    )
+}
+
+const HeadlineTitle = ({ className, children }) => {
+    return (
+        <>
+            <p className={`text-4xl font-bold text-white md:text-5xl ${className}`}>
+                {children}
+            </p>
+        </>
+    )
+}
+
+const HighlightImage = ({ className, image }) => {
+    return (
+        <>
+            <div className={`relative hidden md:block rounded-full overflow-hidden bg-red-300 ${className}`}>
+                <Image
+                    className='relative h-full w-full md:rounded-md overflow-hidden'
+                    asset={image.asset}
+                    objectFit='object-cover'
+                    sizes={[600]}
+                />
+            </div>
+        </>
     )
 }
