8de0d8a093b6014d52cf3961b0b6e6c811bff658
Mustafa <mndarwesh@protonmail.com>
2022-01-27 12:37:34 +0100

    Refactor

--- pages/assortment/index.js
+++ pages/assortment/index.js
@@ -12,27 +12,35 @@ export default function Assortment({ pageData, global, locale }) {
         <Layout data={global}>
             <LandingImage className="mt-20" image={pageData.landingImage} />
             <LandingSectionMain pageData={pageData} />
-            <Main pageData={pageData} />
+            <AssortmentMain pageData={pageData} />
         </Layout>
     )
 }
 
-const Main = ({ pageData }) => {
+const AssortmentMain = ({ pageData }) => {
     return (
         <>
-            <div className='w-full drop-shadow-md bg-white pb-6'>
-                <div className='w-full gap-6 wrapper flex flex-wrap items-center justify-center'>
-                    {pageData.productFilters.map((el) => {
-                        return (
-                            <button className='py-2 px-6 bg-white border-2 rounded-lg border-primary3 text-primary3'>
-                                {el}
-                            </button>
-                        )
-                    })}
-                </div>
-            </div>
-            <ProductSections products={pageData.productFilters} pageData={pageData }/>
+            <FilterButtonRow filters={pageData.productFilters} />
+            <ProductSections products={pageData.productFilters} pageData={pageData} />
         </>
+    )
+}
+
+const FilterButtonRow = ({ filters }) => {
+    return (
+        <div className='w-full drop-shadow-md bg-white pb-6'>
+            <div className='w-full gap-6 wrapper flex flex-wrap items-center justify-center'>
+                {filters.map((filter, index) => <FilterButton key={index} filter={filter} />)}
+            </div>
+        </div>
+    )
+}
+
+const FilterButton = ({ filter, value }) => {
+    return (
+        <button className='py-2 px-6 bg-white border-2 rounded-lg border-primary3 text-primary3'>
+            {filter}
+        </button>
     )
 }
 
@@ -40,11 +48,9 @@ const ProductSections = ({ products, pageData }) => {
     return (
         <div className='bg-[#E0F3FF]'>
             <div className='wrapper w-full grid grid-cols-1 md:grid-cols-2 place-items-center lg:grid-cols-3 py-8 gap-8'>
-                {products.map((el) => {
-                    return (
-                        <ProductCard image={pageData.landingProductImage} />
-                    )
-                })}
+                {products.map((el,index) => 
+                    <ProductCard key={index} image={pageData.landingProductImage}/>
+                )}
             </div>
         </div>
     )
@@ -87,19 +93,14 @@ const LandingSectionMain = ({ pageData }) => {
 }
 
 const MobileLandingSectionMain = ({ pageData, className }) => {
-
     return (
-
         <div className='relative wrapper md:hidden'>
-
-            {/* Product Image */}
             <Image className={`
                 mx-auto w-[70%] max-w-sm aspect-[${pageData.landingProductImage.metadata.dimensions.aspectRatio}]
                 -translate-y-28`}
                 loading='eager'
                 image={pageData.landingProductImage}
             />
-
 
             <Headline1 className="-translate-y-10">
                 {pageData.title}
@@ -114,11 +115,8 @@ const MobileLandingSectionMain = ({ pageData, className }) => {
 }
 
 const TabletLandingSectionMain = ({ pageData, className }) => {
-
     return (
-
         <div className='relative wrapper hidden md:block lg:hidden mb-16'>
-
             <div className='flex items-baseline mt-12 mb-4'>
                 <Headline1 className="flex-2">
                     {pageData.title}
@@ -131,7 +129,6 @@ const TabletLandingSectionMain = ({ pageData, className }) => {
                     />
                 </div>
             </div>
-
             <div className="">
                 <SanityBlockContent
                     blocks={pageData.landingContent} serializers={serializers} />
@@ -143,12 +140,9 @@ const TabletLandingSectionMain = ({ pageData, className }) => {
 const DesktopLandingSectionMain = ({ pageData, className }) => {
     return (
         <div className='relative wrapper hidden lg:grid grid-cols-5 grid-rows-[min-content,auto] mb-16'>
-
             <Headline1 className="col-start-1 col-span-3 py-12">
                 {pageData.title}
             </Headline1>
-
-            {/* Product Image */}
             <div className='col-start-4 col-span-full row-start-1 row-span-full'>
                 <Image className={`relative max-w-full w-96 mx-auto h-full scale-[1.2] -translate-y-1 origin-bottom`}
                     objectFit={"object-contain"}
@@ -156,34 +150,10 @@ const DesktopLandingSectionMain = ({ pageData, className }) => {
                     image={pageData.landingProductImage}
                 />
             </div>
-
-
-
             <div className="row-start-2 col-start-1 col-span-3 ">
                 <SanityBlockContent
                     blocks={pageData.landingContent} serializers={serializers} />
             </div>
-        </div>
-    )
-}
-
-const LandingProductImage = ({ className, image }) => {
-
-    return (
-        <div className={`${className}
-            max-w-[15rem] bg-red-300 aspect-[${image.metadata.dimensions.aspectRatio}] shadow-2xl
-            -translate-y-20
-            md:translate-y-0 md:max-w-none md:w-full
-        `}>
-
-            <Image className={`
-                w-full h-full 
-            `}
-                loading='eager'
-                image={image}
-
-            />
-
         </div>
     )
 }
@@ -212,7 +182,6 @@ export async function getStaticProps(context) {
         pageData = await getClient(context?.preview)
             .fetch(AssortmentPageQuery, { locale: defaultLocale })
     }
-
 
     const global = await getGlobalData(context?.preview, locale, defaultLocale)
 
