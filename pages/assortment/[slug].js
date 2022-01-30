a553bdb929d58f49f39cd81013de36ef14ec1b44
Mustafa <mndarwesh@protonmail.com>
2022-01-30 15:32:07 +0100

    Work on assortment detail page

--- pages/assortment/[slug].js
+++ pages/assortment/[slug].js
@@ -4,14 +4,66 @@ import CollectionSlugs from '../../lib/sanity/queries/CollectionSlugs'
 import ProductDetailPageQuery from '../../lib/sanity/queries/ProductDetailPageQuery'
 import { Headline1 } from '../../components/headlines'
 import Layout from '../../components/layout'
+import Image from '../../components/image'
+import SanityBlockContent from '@sanity/block-content-to-react'
+import { getSerializer } from '../../lib/serializers'
+import ScrollContainer from 'react-indiana-drag-scroll'
+import { useState } from 'react'
 
-export default function Assortment({ pageData, global, locale }) {
+export default function Assortment({ detailData, global, locale }) {
     return (
         <Layout data={global}>
-            <Headline1 className={'wrapper'}>
-                {pageData.title}
-            </Headline1>
+            <LandingSection detailData={detailData} className='mb-12' />
         </Layout>
+    )
+}
+
+const LandingSection = ({ detailData, className }) => {
+    const [selectedImage, setSelectedImage] = useState(detailData.gallary[0]);
+
+
+    return (
+        <div className={`grid ${className}`}>
+            <ProductImage className="mb-6" image={detailData.gallary[0]} />
+            <ImagePicker className='mb-2' gallary={detailData.gallary} />
+            <Headline1 className='wrapper mb-4'>
+                {detailData.title}
+            </Headline1>
+            <div className='wrapper'>
+                <SanityBlockContent
+                    blocks={detailData.description}
+                    serializers={getSerializer()}
+                />
+            </div>
+        </div>
+    )
+}
+
+const ImagePicker = ({ gallary, className }) => {
+    return (
+        <ScrollContainer className={`w-full whitespace-nowrap pb-2 ${className}`}>
+            {gallary.map((el) => {
+                return <button className='w-24 aspect-square first:ml-4 mr-2 last:mr-4 inline-block bg-[#E2E8F3] rounded-md overflow-hidden'>
+                    <Image
+                        image={el}
+                        className={'relative w-full h-full'}
+                        loading='eager'
+                        objectFit='object-cover'
+                    />
+                </button>
+            })}
+        </ScrollContainer>
+    )
+}
+
+const ProductImage = ({ image, className }) => {
+    return (
+        <Image
+            image={image}
+            className={`relative w-full max-w-xs mx-auto ${className}`}
+            loading='eager'
+            aspectRatio={image.metadata.dimensions.aspectRatio}
+        />
     )
 }
 
@@ -31,17 +83,17 @@ export const getStaticPaths = async () => {
 export async function getStaticProps(context) {
     const { locale, defaultLocale, params } = context
 
-    var pageData = await getClient(context?.preview).fetch(ProductDetailPageQuery, { locale, slug: params.slug })
+    var detailData = await getClient(context?.preview).fetch(ProductDetailPageQuery, { locale, slug: params.slug })
 
-    if (!pageData) {
-        pageData = await getClient(context?.preview).fetch(ProductDetailPageQuery, { locale: defaultLocale, slug: params.slug })
+    if (!detailData) {
+        detailData = await getClient(context?.preview).fetch(ProductDetailPageQuery, { locale: defaultLocale, slug: params.slug })
     }
 
     const global = await getGlobalData(context?.preview, locale, defaultLocale)
 
     return {
         props: {
-            pageData,
+            detailData,
             global,
             locale
         }
