dc25a5791e6384fbd1e98a6dc44401457b403c37
Mustafa <mndarwesh@protonmail.com>
2022-01-30 20:48:41 +0100

    Add steps

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -4,16 +4,40 @@ import CollectionSlugs from '../../lib/sanity/queries/CollectionSlugs'
 import { Headline1 } from '../../components/headlines'
 import Layout from '../../components/layout'
 import RecipeDetailPageQuery from '../../lib/sanity/queries/RecipeDetailPageQuery'
+import BannerImage from '../../components/BannerImage'
+import { getSerializer } from '../../lib/serializers'
+import SanityBlockContent from '@sanity/block-content-to-react'
 
-export default function RecipePage({ pageData, global, locale }) {
+export default function RecipePage({ pageDetail, global, locale }) {
+    console.log(pageDetail)
     return (
         <Layout data={global}>
-            <Headline1 className={'wrapper'}>
-                {pageData.title}
-            </Headline1>
+            <div className='wrapper'>
+                <BannerImage className='wrapper mb-12' image={pageDetail.bannerImage} />
+
+                <div className='grid mb-12'>
+                    <Headline1 className='mb-4'>
+                        {pageDetail.title}
+                    </Headline1>
+                    <div className='grid gap-6'>
+                        {pageDetail.steps.map((el, index) => {
+                            return (
+                                <div className={` flex`}>
+                                    <p className=' min-w-[3rem] md:min-w-[4rem] text-3xl text-primary2'>{index + 1}.</p>
+                                    <SanityBlockContent blocks={el.content} serializers={getSerializer()} />
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+
         </Layout>
     )
 }
+
+
 
 export const getStaticPaths = async () => {
     const data = await getClient(false).fetch(CollectionSlugs, { collection: 'recipe' })
@@ -30,17 +54,17 @@ export const getStaticPaths = async () => {
 export async function getStaticProps(context) {
     const { locale, defaultLocale, params } = context
 
-    var pageData = await getClient(context?.preview).fetch(RecipeDetailPageQuery, { locale, slug: params.slug })
+    var pageDetail = await getClient(context?.preview).fetch(RecipeDetailPageQuery, { locale, slug: params.slug })
 
-    if (!pageData) {
-        pageData = await getClient(context?.preview).fetch(RecipeDetailPageQuery, { locale: defaultLocale, slug: params.slug })
+    if (!pageDetail) {
+        pageDetail = await getClient(context?.preview).fetch(RecipeDetailPageQuery, { locale: defaultLocale, slug: params.slug })
     }
 
     const global = await getGlobalData(context?.preview, locale, defaultLocale)
 
     return {
         props: {
-            pageData,
+            pageDetail,
             global,
             locale
         }
