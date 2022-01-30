2cacf5559cd64e4ed1f8dd6a2397e6c9da0f823f
Mustafa <mndarwesh@protonmail.com>
2022-01-30 19:27:28 +0100

    Create LargeLandingSection

--- pages/assortment/[slug].js
+++ pages/assortment/[slug].js
@@ -31,19 +31,83 @@ const LandingSection = ({ detailData, className }) => {
                 selectedImage={selectedImage}
                 onPick={onPick}
                 detailData={detailData} />
+
+            <LargeLandingSection
+                className={className}
+                selectedImage={selectedImage}
+                onPick={onPick}
+                detailData={detailData} />
         </>
 
+    )
+}
+
+const LargeLandingSection = ({ detailData, className, selectedImage, onPick }) => {
+    return (
+        <div className={`md:flex wrapper items-start mt-4 hidden ${className}`}>
+
+            {detailData.gallary.length > 1 && (
+                <ScrollContainer className={`flex flex-col mt-16 lg:mt-20 items-center gap-4  pb-2 ${className}`}>
+                    {detailData.gallary.map((image, index) => {
+                        return <ImagePickerButton
+                            onPick={onPick}
+                            image={image}
+                            key={index}
+                        />
+                    })}
+                </ScrollContainer>
+            )}
+
+            <Image
+                image={selectedImage}
+                className={` relative flex-1 rounded-md overflow-hidden ${className}`}
+                loading='eager'
+                aspectRatio={selectedImage.metadata.dimensions.aspectRatio}
+            />
+
+            <div className='flex-1 self-start'>
+                <Headline1 className='mb-4 !text-4xl'>
+                    {detailData.title}
+                </Headline1>
+
+                <SanityBlockContent
+                    blocks={detailData.description}
+                    serializers={getSerializer()}
+                />
+            </div>
+        </div>
     )
 }
 
 const SmallLandingSection = ({ detailData, className, selectedImage, onPick }) => {
     return (
         <div className={`grid md:hidden ${className}`}>
-            <ProductImage className="mb-6" image={selectedImage} />
-            <ImagePicker className='mb-2' onPick={onPick} gallary={detailData.gallary} />
+            <Image
+                image={selectedImage}
+                className={`mb-6 relative h-60 rounded-md overflow-hidden w-full ${className}`}
+                loading='eager'
+                objectFit='object-contain'
+            />
+
+            {detailData.gallary.length > 1 && (
+                <ScrollContainer className={`w-full grid place-items-center whitespace-nowrap pb-2 ${className}`}>
+                    <div>
+                        {detailData.gallary.map((image, index) => {
+                            return <ImagePickerButton
+                                onPick={onPick}
+                                image={image}
+                                key={index}
+                                className='mr-2 first:ml-4 last:mr-4'
+                            />
+                        })}
+                    </div>
+                </ScrollContainer>
+            )}
+
             <Headline1 className='wrapper mb-4'>
                 {detailData.title}
             </Headline1>
+
             <div className='wrapper'>
                 <SanityBlockContent
                     blocks={detailData.description}
@@ -54,28 +118,11 @@ const SmallLandingSection = ({ detailData, className, selectedImage, onPick }) =
     )
 }
 
-const ImagePicker = ({ gallary, className, onPick }) => {
-    return (
-        <ScrollContainer className={`w-full grid place-items-center whitespace-nowrap pb-2 ${className}`}>
-            <div>
-                {gallary.map((image, index) => {
-                    return <ImagePickerButton
-                        onPick={onPick}
-                        image={image}
-                        key={index}
-                        className='mr-2 first:ml-4 last:mr-4'
-                    />
-                })}
-            </div>
-        </ScrollContainer>
-    )
-}
-
 const ImagePickerButton = ({ className, image, onPick }) => {
     return (
         <button className={`
             ${className}
-            w-24 aspect-square bg-[#E2E8F3] rounded-md overflow-hidden`}
+            w-24 bg-orange-100 aspect-square rounded-md overflow-hidden`}
             onClick={(e) => onPick(image)}
         >
             <Image
@@ -85,17 +132,6 @@ const ImagePickerButton = ({ className, image, onPick }) => {
                 objectFit='object-cover'
             />
         </button>
-    )
-}
-
-const ProductImage = ({ image, className }) => {
-    return (
-        <Image
-            image={image}
-            className={`relative h-60 rounded-md overflow-hidden w-full ${className}`}
-            loading='eager'
-            objectFit='object-contain'
-        />
     )
 }
 
