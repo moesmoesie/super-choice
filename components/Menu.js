ec954085598f1c81d8267224cbc21a5549d4f244
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-02-02 00:20:04 +0100

    Clean up menu animation

--- components/Menu.js
+++ components/Menu.js
@@ -1,25 +1,41 @@
 import { motion, AnimatePresence } from "framer-motion"
-import Link from "next/link";
 import { useContext } from "react";
 import AppContext from "../lib/contexts/AppContext";
 
 export default function Menu({ data }) {
     const { isMenuOpen } = useContext(AppContext);
 
+    const variants = {
+        visible: {
+            translateY: '0px',
+            transition: {
+                type: 'easeOut',
+            }
+        },
+        hidden: { 
+            translateY: '-100%',
+            transition: {
+                type: 'easeOut',
+                when: "afterChildren",
+            }
+        },
+    }
+
+
+
     return (
         <>
             <AnimatePresence>
                 {isMenuOpen && (
                     <motion.div
-                        initial={{ translateY: '-100%' }}
-                        animate={{ translateY: '0px' }}
-                        exit={{ translateY: '-100%'}}
-                        transition={{ type: 'easeOut', duration: 0.3 }}
+                        initial='hidden'
+                        animate='visible'
+                        exit='hidden'
+                        variants={variants}
                         className={`w-full absolute top-0 bg-primary5`}
                     >
                         <div className="wrapper mt-32 mb-32">
-                            <MenuList className="mb-8" />
-                            <MenuLine />
+                            <MenuList />
                         </div>
                     </motion.div>)}
             </AnimatePresence>
@@ -27,34 +43,10 @@ export default function Menu({ data }) {
     )
 }
 
-const BottomLinks = () => {
-    return (
-        <div className="flex">
-            <Link href={flex}>
-            </Link>
-
-            <Link>
-            </Link>
-        </div>
-    )
-}
-
-const MenuLine = () => {
-    return (
-        <motion.div
-            transition={{delay: 1, type: 'easeOut'}}
-            exit={{width: "0%"}}
-            initial={{ width: '0%' }}
-            animate={{ width: '100%' }}
-            className="h-[1px] bg-white/20"
-        />
-    )
-}
-
 const MenuList = ({className}) => {
     const container = {
         hidden: { opacity: 0 },
-        show: {
+        visible: {
             opacity: 1,
             transition: {
                 delay: 0.3,
@@ -67,8 +59,6 @@ const MenuList = ({className}) => {
     return (
         <motion.ul
             variants={container}
-            initial="hidden"
-            animate="show"
             className={`grid gap-5 ${className}`}>
             {[1, 2, 3, 4, 5].map((el, index) => {
                 return <MenuItem key={index} index={index} />
@@ -80,7 +70,7 @@ const MenuList = ({className}) => {
 const MenuItem = ({ index }) => {
     const item = {
         hidden: { opacity: 0, translateX: "-40px", transition: { type: 'easeIn' } },
-        show: { opacity: 1, translateX: "0px", transition: { type: 'easeIn' } }
+        visible: { opacity: 1, translateX: "0px", transition: { type: 'easeIn' } }
     }
 
     return (
