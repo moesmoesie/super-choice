7a84aec1df199a6171a6a10d9e1acf3148a9f435
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-02-05 22:16:44 +0100

    Refactor news page

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,119 +1,19 @@
 import { getGlobalData, getClient } from '../../lib/sanity/sanity.server'
 import React from "react"
 import NewsPageQuery from '../../lib/sanity/queries/NewsPageQuery'
-import { Headline1 } from '../../components/headlines'
-import BannerImage from '../../components/BannerImage'
-import SanityBlockContent from '@sanity/block-content-to-react'
-import { getSerializer } from '../../lib/serializers'
-import FilterRow from '../../components/FilterRow'
-import { useState } from 'react'
-import Link from 'next/link'
-import Image from '../../components/image'
-import { useFilterCollection } from '../../lib/hooks/useFilterCollection'
-import CollectionGrid from '../../components/CollectionGrid'
-import { motion, AnimatePresence } from 'framer-motion'
 import Seo from '../../components/Seo'
-import Page from '../../components/Page'
-const PageContext = React.createContext();
+import NewsPage from '../../lib/pages/News/Index'
 
 export default function News({ pageData, global, preview, locale }) {
     return (
         <>
             <Seo seo={pageData.seo} />
-            <Page preview={preview} data={global}>
-                <LandingSection className="mb-12" pageData={pageData} />
-                <MainSection pageData={pageData} />
-            </Page>
+            <NewsPage 
+                pageData={pageData} 
+                global={global} 
+                preview={preview}
+            />
         </>
-    )
-}
-
-const MainSection = ({ pageData }) => {
-    const [selectedFilter, setSelectedFilter] = useState(null);
-    const { data } = useFilterCollection({ filter: selectedFilter, collection: pageData.articles })
-
-    function onFilterClick(value) {
-        setSelectedFilter(selectedFilter == value ? null : value)
-    }
-
-    return (
-        <PageContext.Provider value={{ selectedFilter, setSelectedFilter }}>
-            <FilterRow
-                onClick={onFilterClick}
-                currentFilter={selectedFilter}
-                filters={pageData.articleFilters} />
-            <ArticleSection articles={data ? data : []} pageData={pageData} />
-        </PageContext.Provider>
-    )
-}
-
-const ArticleSection = ({ articles }) => {
-
-    return (
-        <CollectionGrid>
-            <AnimatePresence initial={false}>
-
-                {articles.map((article, index) =>
-                    <ArticleCard
-                        index={index}
-                        key={article.key}
-                        article={article}
-                        cta="Lees meer" />
-                )}
-            </AnimatePresence>
-
-        </CollectionGrid>
-    )
-}
-
-const ArticleCard = ({ article, index, cta }) => {
-    return (
-        <Link passHref href={`/news/${article.slug}`}>
-            <motion.a
-                exit={{ opacity: 0, translateX: -20 }}
-                initial={{ opacity: 0, translateX: 20 }}
-                animate={{ opacity: 1, translateX: 0, transition: { delay: 0.1 * index } }}
-
-                className='w-full z-20 h-full flex min-h-[32rem] flex-col bg-white overflow-hidden rounded-md cardShadow group'>
-                <div className='overflow-hidden h-80 mb-6 w-full'>
-                    <Image
-                        className="relative group-hover:scale-110 h-full duration-300 w-full  "
-                        image={article.previewImage}
-                        objectFit='object-cover'
-                        mediaQueries={[
-                            { w: 1, s: 500 },
-                            { w: 500, s: 1000 },
-                            { w: 1000, s: 1500 }
-                        ]}
-                    />
-                </div>
-
-                <div className='flex flex-1 flex-col pl-6 pr-6 pb-8'>
-                    <p className='text-primary3 font-medium font-header text-3xl truncate'>{article.title}</p>
-                    <p className='text-[#8D8F94] font-header mb-6'>12 feb 2022</p>
-                    <div className='mb-8'>
-                        <SanityBlockContent
-                            blocks={article.summary} serializers={getSerializer()} />
-                    </div>
-                    <button className={`button2 duration-300 mt-auto group-hover:text-white group-hover:bg-primary3`}>
-                        {cta}
-                    </button>
-                </div>
-            </motion.a>
-        </Link>
-    )
-}
-
-const LandingSection = ({ pageData, className }) => {
-    return (
-        <div className={`wrapper grid lg:grid-rows-[min-content,auto] lg:grid-cols-2 ${className}`}>
-            <BannerImage className="mb-14 lg:col-start-2 lg:h-full lg:col-span-full lg:row-span-2 lg:mb-0" image={pageData.landingImage} />
-            <Headline1 className="mb-12 lg:my-12 lg:row-start-1">{pageData.title}</Headline1>
-            <div className='lg:col-start-1 lg:pb-12 lg:max-w-[80%]'>
-                <SanityBlockContent
-                    blocks={pageData.landingContent} serializers={getSerializer()} />
-            </div>
-        </div>
     )
 }
 
