0eae72aaf2b6a1bf52967cbac696c79fac0d3f14
Mustafa <mndarwesh@protonmail.com>
2022-02-06 01:32:37 +0100

    Rework article page

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,65 +1,41 @@
 import { getGlobalData, getClient } from '../../lib/sanity/sanity.server'
-import React from "react"
 import CollectionSlugs from '../../lib/sanity/queries/CollectionSlugs'
-import ArticleDetailPageQuery from '../../lib/sanity/queries/ArticleDetailPageQuery'
-import { Headline1 } from '../../components/headlines'
-import BannerImage from '../../components/BannerImage'
-import SanityBlockContent from '@sanity/block-content-to-react'
-import { getSerializer } from '../../lib/serializers'
-import Link from 'next/link'
-import Image from '../../components/image'
+import { getRichImageQuery,getRichTextEditorQuery } from '../../lib/sanity/components'
 import Seo from '../../components/Seo'
-import Page from '../../components/Page'
+import ArticleDetail from '../../lib/pages/ArticleDetail/Index'
 
-export default function NewsArticle({ pageDetail, global, preview, locale }) {
+export default function NewsArticle({ detailData, global, preview, locale }) {
     return (
         <>
-            <Seo seo={pageDetail.seo} />
-            <Page preview={preview} data={global}>
-                <div className='wrapper mb-12'>
-                    <BannerImage className='wrapper mb-12' image={pageDetail.bannerImage} />
-                    <div className='lg:gap-20 grid relative lg:grid-cols-[auto,20rem]'>
-                        <div>
-                            <Headline1 className='mb-4'>
-                                {pageDetail.title}
-                            </Headline1>
-                            <SanityBlockContent blocks={pageDetail.content} serializers={getSerializer()} />
-                        </div>
-                        <div className='self-start'>
-                            <p className='text-3xl mt-8 mb-4 font-header font-bold text-primary4'>Latest Posts</p>
-                            <div className='grid md:grid-cols-2 lg:grid-cols-1 gap-4'>
-                                {pageDetail.latestPosts.map((article, index) => {
-                                    return (
-                                        <Link key={index} href={`/news/${article.slug}`}>
-                                            <a className='flex gap-4 relative items-center'>
-                                                <Image
-                                                    image={article.previewImage}
-                                                    objectFit='object-cover'
-                                                    className=' relative brightness-75 overflow-x-hidden min-w-[6rem] w-24 md:h-44 lg:w-24 lg:h-24 md:w-full h-24 bg-red-500 rounded-md'
-                                                    mediaQueries={[
-                                                        { w: 1, s: 500 },
-                                                        { w: 500, s: 1000 },
-                                                        { w: 1000, s: 1500 }
-                                                    ]}
-                                                />
-                                                <p className='text-primary4 font-semibold text-xl 
-                                                md:absolute md:text-white md:left-1/2 md:top-1/2 md:-translate-y-1/2 md:-translate-x-1/2
-                                                lg:relative lg:left-auto lg:top-auto lg:translate-y-0 lg:translate-x-0 lg:text-primary4
-                                        '>
-                                                    {article.title}</p>
-                                            </a>
-                                        </Link>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Page>
+            <Seo seo={detailData.seo}/>
+            <ArticleDetail
+                detailData={detailData}
+                global={global}
+                preview={preview}
+            />
         </>
     )
 }
 
+const query = `
+    *[
+        _type == "article" && 
+        slug.current == $slug && 
+        language->languageCode == $locale][0]{
+            _id,
+            title,
+            seo,
+            ${getRichTextEditorQuery('content')},
+            ${getRichImageQuery('bannerImage')},
+            'latestPosts' : *[_type == "newsPage" && language->languageCode == $locale][0]{
+                'articles': articles[0...4]->{
+                    title,
+                    'slug': slug.current,
+                    ${getRichImageQuery('previewImage')}
+                }
+            }.articles
+        }
+`
 
 export const getStaticPaths = async () => {
     const data = await getClient(false).fetch(CollectionSlugs, { collection: 'article' })
@@ -76,17 +52,17 @@ export const getStaticPaths = async () => {
 export async function getStaticProps(context) {
     const { locale, defaultLocale, params } = context
 
-    var pageDetail = await getClient(context?.preview).fetch(ArticleDetailPageQuery, { locale, slug: params.slug })
+    var detailData = await getClient(context?.preview).fetch(query, { locale, slug: params.slug })
 
-    if (!pageDetail) {
-        pageDetail = await getClient(context?.preview).fetch(ArticleDetailPageQuery, { locale: defaultLocale, slug: params.slug })
+    if (!detailData) {
+        detailData = await getClient(context?.preview).fetch(query, { locale: defaultLocale, slug: params.slug })
     }
 
     const global = await getGlobalData(context?.preview, locale, defaultLocale)
 
     return {
         props: {
-            pageDetail,
+            detailData,
             global,
             locale,
             'preview': context.preview ?? false,
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,33 +1,55 @@
 import { getGlobalData, getClient } from '../../lib/sanity/sanity.server'
 import React from "react"
-import NewsPageQuery from '../../lib/sanity/queries/NewsPageQuery'
 import Seo from '../../components/Seo'
 import NewsPage from '../../lib/pages/News/Index'
+import { getRichImageQuery, getRichTextEditorQuery } from '../../lib/sanity/components'
 
 export default function News({ pageData, global, preview, locale }) {
     return (
         <>
             <Seo seo={pageData.seo} />
-            <NewsPage 
-                pageData={pageData} 
-                global={global} 
+            <NewsPage
+                pageData={pageData}
+                global={global}
                 preview={preview}
             />
         </>
     )
 }
 
+const query = `
+    *[_type == "newsPage" && language->languageCode == $locale][0]{
+        _id,
+        title,
+        seo,
+        'articleFilters' : articleFilters[]{
+            value,
+            title
+        },
+        ${getRichTextEditorQuery('landingContent')},
+        ${getRichImageQuery('landingImage')},
+        'articles' : articles[]->{
+            _id,
+            'slug' : slug.current,
+            title,
+            catagories,
+            ${getRichImageQuery('previewImage')},
+            ${getRichTextEditorQuery('summary')},
+            ${getRichImageQuery('bannerImage')}
+        }
+    }
+`
+
 export async function getStaticProps(context) {
     const { locale, defaultLocale } = context
 
-    var pageData = await getClient(context?.preview).fetch(NewsPageQuery, { locale })
+    var pageData = await getClient(context?.preview).fetch(query, { locale })
 
     if (!pageData) {
-        pageData = await getClient(context?.preview).fetch(NewsPageQuery, { locale: defaultLocale })
+        pageData = await getClient(context?.preview).fetch(query, { locale: defaultLocale })
     }
 
     const global = await getGlobalData(context?.preview, locale, defaultLocale)
-
     return {
         props: {
             pageData,
