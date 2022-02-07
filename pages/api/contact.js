8b5fcf2089cef2b45c957326b13e0a1557090aaf
Mustafa <mndarwesh@protonmail.com>
2022-02-07 20:19:42 +0100

    Change template

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -15,7 +15,7 @@ export default async function (req, res) {
             from: 'super.choice.contact@gmail.com',
             to: 'super.choice.contact@gmail.com',
             subject: `Message From ${req.body?.firstName} ${req.body?.lastName}`,
-            text: req.body?.message + " | Sent from: " + req.body?.email,
+            text: req.body?.message + " | Sent from:" + req.body?.email,
             html: `<div>${req.body?.message}</div><p>Sent from: ${req.body?.email}</p>`
         }
 
