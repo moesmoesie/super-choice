3c8489787ece8e6ee112e8207a14ad8ce5111250
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-06-08 16:15:37 +0200

    Add posthog implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
-import '../styles/globals.css'
-import { useState, useEffect } from 'react';
-import AppContext from '../lib/contexts/AppContext';
-import Cookies from 'js-cookie'
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import AppContext from "../lib/contexts/AppContext";
+import Cookies from "js-cookie";
+import { usePostHog } from "next-use-posthog";
 
 function MyApp({ Component, pageProps }) {
+  usePostHog("phc_SjdBJsaSM7aFlJzrIM3l3b0rGg8IAU1kQB6IWyJuVvh", {
+    api_host: "https://app.posthog.com",
+  });
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [hasCookies, setCookies] = useState(false);
   useEffect(() => {
-    if(Cookies.get('hasSuperChoiceCookies')){
-      setCookies(true)
+    if (Cookies.get("hasSuperChoiceCookies")) {
+      setCookies(true);
     }
-  },[hasCookies]);
-
+  }, [hasCookies]);
 
   return (
     <AppContext.Provider
@@ -19,12 +23,12 @@ function MyApp({ Component, pageProps }) {
         isMenuOpen: isMenuOpen,
         setIsMenuOpen: setIsMenuOpen,
         hasCookies: hasCookies,
-        setCookies: setCookies
+        setCookies: setCookies,
       }}
     >
       <Component {...pageProps} />
     </AppContext.Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
